bea56f47417a8b78e434b84e5224e180fc2908cc
oliveirakcs <oliveirakcs@icloud.com>
2024-08-07 10:58:19 -0300

    Hotfix on Auth Tests

--- app/tests/integration/auth/test_auth.py
+++ app/tests/integration/auth/test_auth.py
@@ -2,60 +2,18 @@
 Integration tests for the login endpoint in the authentication system.
 """
 
-import pytest
-from fastapi.testclient import TestClient
-from sqlalchemy.orm import Session
-from app.main import app
-from app.infrastructure.database.models import User
-from app.infrastructure.database import get_db
-from app.core.auth.hashing import Hash
-from app.scripts import External
+import os
+from app.tests import create_client
 
-client = TestClient(app)
+client = create_client()
 
 
-@pytest.fixture(scope="function")
-def test_db_session():
-    """
-    Fixture for setting up a test database session.
-    """
-    db = next(get_db())
-    yield db
-    db.rollback()
-
-
-@pytest.fixture(scope="function")
-def test_user(test_db_session: Session):
-    """
-    Fixture for setting up a test user in the database.
-
-    Args:
-        test_db_session (Session): The test database session.
-
-    Returns:
-        User: The created test user.
-    """
-    user_data = External.fetch_random_user()
-    hashed_password = Hash.bcrypt("password123")
-    user = User(
-        username=user_data["username"],
-        email=user_data["email"],
-        name=user_data["name"],
-        password=hashed_password,
-        role="user",
-    )
-    test_db_session.add(user)
-    test_db_session.commit()
-    test_db_session.refresh(user)
-    return user
-
-
-def test_login_success(test_user):
+def test_login_success():
     """
     Test the login endpoint for successful authentication.
 
     Args:
-        test_user (User): The test user fixture.
+        user (User): The test user fixture.
 
     Test steps:
     1. Send a POST request to the login endpoint with valid credentials.
@@ -63,8 +21,7 @@ def test_login_success(test_user):
     3. Check the presence of an access token in the response.
     """
     response = client.post(
-        "/api/v1/login/",
-        data={"username": test_user.username, "password": "password123"},
+        url="/api/v1/login/", data={"username": os.environ.get("SYSADMIN_USERNAME"), "password": os.environ.get("SYSADMIN_PASSWORD")}
     )
     assert response.status_code == 200
     assert "access_token" in response.json()
@@ -89,12 +46,12 @@ def test_login_invalid_username(access_token):
     assert response.json()["detail"] == "Invalid credentials"
 
 
-def test_login_invalid_password(test_user):
+def test_login_invalid_password(access_token, user):
     """
     Test the login endpoint with an invalid password.
 
     Args:
-        test_user (User): The test user fixture.
+        user (User): The test user fixture.
 
     Test steps:
     1. Send a POST request to the login endpoint with an invalid password.
@@ -102,13 +59,14 @@ def test_login_invalid_password(test_user):
     """
     response = client.post(
         "/api/v1/login/",
-        data={"username": test_user.username, "password": "wrongpassword"},
+        data={"username": user["username"], "password": "wrongpassword"},
+        headers={"Authorization": f"Bearer {access_token}"},
     )
     assert response.status_code == 401
     assert response.json()["detail"] == "Incorrect password"
 
 
-def test_login_empty_credentials():
+def test_login_empty_credentials(access_token):
     """
     Test the login endpoint with empty credentials.
 
@@ -116,16 +74,16 @@ def test_login_empty_credentials():
     1. Send a POST request to the login endpoint with empty credentials.
     2. Verify the response status code is 422 (Unprocessable Entity).
     """
-    response = client.post("/api/v1/login/", data={"username": "", "password": ""})
+    response = client.post("/api/v1/login/", data={"username": "", "password": ""}, headers={"Authorization": f"Bearer {access_token}"})
     assert response.status_code == 422
 
 
-def test_login_without_password(test_user):
+def test_login_without_password(user, access_token):
     """
     Test the login endpoint without a password.
 
     Args:
-        test_user (User): The test user fixture.
+        user (User): The test user fixture.
 
     Test steps:
     1. Send a POST request to the login endpoint without a password.
@@ -133,12 +91,13 @@ def test_login_without_password(test_user):
     """
     response = client.post(
         "/api/v1/login/",
-        data={"username": test_user.username, "password": ""},
+        data={"username": user["username"], "password": ""},
+        headers={"Authorization": f"Bearer {access_token}"},
     )
     assert response.status_code == 422
 
 
-def test_login_without_username():
+def test_login_without_username(access_token):
     """
     Test the login endpoint without a username.
 
@@ -149,5 +108,6 @@ def test_login_without_username():
     response = client.post(
         "/api/v1/login/",
         data={"username": "", "password": "password123"},
+        headers={"Authorization": f"Bearer {access_token}"},
     )
     assert response.status_code == 422
